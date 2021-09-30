34b00bfb2cac58de3e0df610589acb478b492f82
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-30 12:43:04 +0800

    removed variable response since it is not necessary

--- app/api/rawg/client.rb
+++ app/api/rawg/client.rb
@@ -4,27 +4,27 @@ module Rawg
   # using RAWG API
   class Client
     def self.creator_roles
-      response = Request.call('get', '/creator-roles')
+      Request.call('get', '/creator-roles')
     end
 
     def self.developers
-      response = Request.call('get', '/developers')
+      Request.call('get', '/developers')
     end
 
     def self.games
-      response = Request.call('get', '/games')
+      Request.call('get', '/games')
     end
 
     def self.genres
-      response = Request.call('get', '/genres')
+      Request.call('get', '/genres')
     end
 
     def self.platforms
-      response = Request.call('get', '/platforms')
+      Request.call('get', '/platforms')
     end
 
     def self.parents_platforms
-      response = Request.call('get', '/platforms/lists/parents')
+      Request.call('get', '/platforms/lists/parents')
     end
   end
 end
