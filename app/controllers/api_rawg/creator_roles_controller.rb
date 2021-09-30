9576feb23455aad56227606ccc3a58abd75c3263
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-30 20:56:18 +0800

    creators-roles controller

--- app/controllers/api_rawg/creator_roles_controller.rb
+++ app/controllers/api_rawg/creator_roles_controller.rb
@@ -6,7 +6,7 @@ module ApiRawg
     def index
       creator_roles = Rawg::Client.creator_roles
 
-      render json: creator_roles #, only: [:name]
+      render json: creator_roles # , only: [:name]
     end
   end
 end
