c514d85424fe4be2c7101571c0d4dbe48db3445e
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-01 16:31:42 +0800

    updated bootstrap

--- app/controllers/api_rawg/creator_roles_controller.rb
+++ app/controllers/api_rawg/creator_roles_controller.rb
@@ -5,8 +5,11 @@ module ApiRawg
   class CreatorRolesController < ApplicationController
     def index
       creator_roles = Rawg::Client.creator_roles
-
-      render json: creator_roles # , only: [:name]
+      creator_roles.each do |creator|
+      
+      end
+      render json: creator_roles
+      # render json: creator_roles # , only: [:name]
     end
   end
 end
